31746ff3513a2fe9a0c75d5dca088a5e33dfb2bb
Nunf <53783537+Nunf@users.noreply.github.com>
2021-10-18 13:56:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,48 @@
 <?php
 // Налаштування для математичної функції h
-const MATH_FUNC_H_ARG_A 		= 4.26; 			// значення агрументу a
-const MATH_FUNC_H_ARG_B 		= 7.29; 			// значення агрументу b
-const MATH_FUNC_H_COND_ARG_A 		= 8.3;				// значення агрументу a в умові
-const MATH_FUNC_H_COND_ARG_B 		= 6.8;				// значення агрументу b в умові
-const MATH_FUNC_H_RESULT_TEXT 		= "Математична функція h є ";	// Текст результату
-const MATH_FUNC_H_RESULT_TRUE_TEXT 	= "дійсною";			// Текст результату дійсності
-const MATH_FUNC_H_RESULT_FALSE_TEXT 	= "не дійсною<br>";		// Текст результату не дійстності
+const MATH_FUNC_H_ARG_A 			= 8.3; 							// значення агрументу a
+const MATH_FUNC_H_ARG_B 			= 6.8; 							// значення агрументу b
+const MATH_FUNC_H_ARGS_X			= array(4.26, 7.29);					// значення аргументу x для умов
 
 // Налаштування для математичної функції z
-const MATH_FUNC_Z_ARG_A 		= 1.6; 							// значення агрументу a
-const MATH_FUNC_Z_ARG_B 		= 14.3; 						// значення агрументу b
-const MATH_FUNC_Z_MIN_ARG_RANGE_T 	= 2.75;							// мінімальне значення діапазону для аргументу t
-const MATH_FUNC_Z_MAX_ARG_RANGE_T 	= 5.0;							// максимальне значення діапазону для аргументу t
-const MATH_FUNC_Z_ITERATIONS_COUNT 	= 9;							// кількість ітерацій параметру t
-const MATH_FUNC_Z_RESULT_TEXT 		= "Значення аргументу t - результат функції:<br>";	// Текст результату
+const MATH_FUNC_Z_ARG_A 			= 1.6; 							// значення агрументу a
+const MATH_FUNC_Z_ARG_B 			= 14.3; 						// значення агрументу b
+const MATH_FUNC_Z_MIN_ARG_RANGE_T 		= 2.75;							// мінімальне значення діапазону для аргументу t
+const MATH_FUNC_Z_MAX_ARG_RANGE_T 		= 5.0;							// максимальне значення діапазону для аргументу t
+const MATH_FUNC_Z_ITERATIONS_COUNT 		= 9;							// кількість ітерацій параметру t
+const MATH_FUNC_Z_RESULT_TEXT 			= "<br>Значення аргументу t - результат функції:<br>";	// Текст результату
 
-echo MATH_FUNC_H_RESULT_TEXT . (f_mathFuncH(	MATH_FUNC_H_ARG_A,
-						MATH_FUNC_H_ARG_B,
-						MATH_FUNC_H_COND_ARG_A,
-						MATH_FUNC_H_COND_ARG_B) ? 	MATH_FUNC_H_RESULT_TRUE_TEXT :
-										MATH_FUNC_H_RESULT_FALSE_TEXT);
+f_mathFuncH(MATH_FUNC_H_ARG_A, MATH_FUNC_H_ARG_B, MATH_FUNC_H_ARGS_X);
 
 f_printMathFuncZ();
 
-function f_mathFuncH($argA, $argB, $condArgA, $condArgB)
+function f_mathFuncH($argA, $argB, $argsX)
 {
-	$results = array(2);
-
-	$results[0] = log($argA + $argB, 2) * $argB;
-	$results[1] = log(pow($argB, 2) + $argA / $argB);
-
-	if($results[0] <= $condArgB)
+	foreach(MATH_FUNC_H_ARGS_X as $argX)
 	{
-		if($condArgA >= results[1] && results[1] > $condArgB)
+		if($argX <= MATH_FUNC_H_ARG_B)
 		{
-			return true;
+			echo pow(log10($argA + $argB), 2) * $argB;
+			echo "<br>";
+		}
+		else if(MATH_FUNC_H_ARG_A >= $argX && $argX > $MATH_FUNC_H_ARG_B)
+		{
+			echo log(pow($argB, 2) + $argA / $argB);
+			echo "<br>";
 		}
 	}
-
-	return false;
 }
 
 function f_mathFuncZ($argA, $argB, $minArgRangeT, $maxArgRangeT, $iterationsCount)
 {
 	$stepSize = ($maxArgRangeT - $minArgRangeT) / $iterationsCount;
-	$argT = $minArgRangeT;
+	$argT = $minArgRangeT + $stepSize;
 	$results = array(array(2));
 
 	for($i = 0; $i < $iterationsCount; $i++)
 	{
 		$results[$i][0] = $argT;
-		$results[$i][1] = log(exp($argT - $argA) + pow($argB, $argA)) * $argB / 2;
+		$results[$i][1] = log10(exp($argT - $argA) + pow($argB, $argA)) * $argB / 2;
 		$argT += $stepSize;
 	}
 
